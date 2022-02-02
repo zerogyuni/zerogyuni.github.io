bd6954217048ca04e805fd4de3b5cc114e93b49f
watson <watson.lee@tosslab.com>
2022-02-02 14:14:55 +0900

    Init index page

---
+++ site-meta-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    title: `LEE YEONG GYUN`,
+    subtitle: `Android Developer Blog`,
+    description: `Android Developer Blog`,
+    author: {
+        name: `@LEE-YEONG-GYUN`,
+        summary: `live in Seoul, Korea`,
+    },
+    language: `ko`,
+    introduction: `Hello World!`,
+    siteUrl: `https://zerogyuni.github.io/`,
+    social: {
+        github: `zerogyunis@gmail.com`,
+        medium: ``,
+        facebook: ``,
+        linkedin: ``,
+        instagram: `zerogyuni`,
+    },
+    icon: `content/assets/logo_haong.png`, // Add your favicon
+    keywords: [
+        `blog`,
+        `gatsby`,
+        `application`,
+        `android`,
+        `YEONGGYUN`,
+        `Watson`,
+        `Mobile`,
+        `Developer`,
+        `LEEYEONGGYUN`,
+    ],
+    comment: {
+        utterances: 'zerogyuni/myblog', // Your repository for archive comment
+    },
+}
